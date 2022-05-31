f2e6dad15bd3cf207ea4a52be2401420b22e1369
Leon <cohenleonlbc@gmail.com>
2022-05-30 23:48:10 -0300

    remove reset

--- src/main/resources/reset.sql
+++
@@ -1,5 +0,0 @@
---Apaga todos os dados de moeda e cotacao
-DELETE FROM espm.moeda;
-DELETE FROM espm.cotacao;
---Rodar no Mysql Command Line Client p/ Nao ter que
---      mudar configuracao de segurança do Workbench
