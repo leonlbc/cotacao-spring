787c02b9003951b18ca9a76e84be961f45ea2e22
Leon <cohenleonlbc@gmail.com>
2022-06-05 20:07:17 -0300

    db name update

--- src/main/java/br/espm/cambio/CambioResource.java
+++ src/main/java/br/espm/cambio/CambioResource.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,12 @@
 
-CREATE TABLE espm.moeda (
+CREATE TABLE cambioapp.moeda (
 	id_moeda varchar(40) not null,
     tx_nome varchar(50) not null,
     tx_simbolo varchar(5) not null,
     primary key pk_moeda (id_moeda)
 );
 
-CREATE TABLE espm.cotacao(
+CREATE TABLE cambioapp.cotacao(
     id_cotacao varchar(64) not null,
     id_moeda varchar(64) not null,
     dt_data date not null,
